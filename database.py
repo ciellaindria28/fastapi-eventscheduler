ea9a06f62e41d446c1a9149b8067f21e58eac40b
Graciella Regina <74176164+ciellaindria28@users.noreply.github.com>
2023-03-11 14:58:37 +0700

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL ="sqlite:///./events.db"
-SQLALCHEMY_DATABASE_URL ="postgresql://postgres:LAW12345@35.238.9.13:5432/eventdatabase"
+SQLALCHEMY_DATABASE_URL ="postgresql://postgres:LAW12345@199.223.232.247:5432/eventdatabase"
 
 # engine = create_engine(
 #     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False}
